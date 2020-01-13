25e57dc9ed6a503c1e4c9b8779e63cc3a34542c3
Star <starkovskyi@protonmail.com>
2020-01-13 14:50:01 +0300

    correct mapKey

--- app/src/main/java/com/dev/recyclica/api/AppConfig.java
+++ app/src/main/java/com/dev/recyclica/api/AppConfig.java
@@ -2,6 +2,6 @@ package com.dev.recyclica.api;
 
 public class AppConfig {
     public static final String BASE_URL = "https://dyn.pearx.net:8080";
-    public static final String MAP_KEY = "411aee7a-843a-4fc8-99d4-0ceecd47762e";
+    public static final String MAP_KEY = "b4c32dd3-4199-4b53-b8e1-dda31ec96cd0";
 
 }
